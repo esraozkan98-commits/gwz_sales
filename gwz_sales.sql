cad31aab403528d48039cfbfa199a2232a335eb6
esraozkan98-commits <esraozkan98@gmail.com>
2026-01-29 22:30:46 +0300

    Update gwz_sales.sql
    Round revenue and purchase cost to 2 decimals

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,7 @@
 SELECT
   date_date AS order_day,
-  SUM(turnover) AS daily_revenue,
-  SUM(purchase_cost) AS daily_purchase_cost
+  ROUND(SUM(turnover), 2) AS daily_revenue,
+  ROUND(SUM(purchase_cost), 2) AS daily_purchase_cost
 FROM `data-analytics-469406.course14.gwz_sales`
 GROUP BY order_day
 ORDER BY order_day;
